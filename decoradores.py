3c23a90a922be4dcbd29d3b80cca6ac67212d34b
Javier Ponce <javiertecnoexit@gmail.com>
2022-05-03 21:35:35 -0300

    tarea finalizada2

--- decoradores.py
+++ decoradores.py
@@ -5,6 +5,7 @@ la cual a través de un decorator poder imprimir el texto “Hola” y “Chau�
 Hola
 Esto es Programación Avanzada.
 Chau
+
 Programar un decorador que en caso de querer dividir por 0 emita un mensaje por pantalla. Integrar en un ejemplo
 
 Programar un decorador para que imprima la fecha y hora. Integrar en un ejemplo.
@@ -16,45 +17,129 @@ Investigar: i) Cómo invocar a 2 decoradores a la vez?
 
 # ejercicio 2 a
 
+print("Ejercicio 2.a")
+#declaracion de la funcion decoradora
 def decorador_de_mensaje(function):
     
-    def funcion_de_retorno():
-        print("Hola")
-        function()
-        print("Chau")
+    def funcion_de_retorno():# declaracion de la funcion de retorno o funcion envolvente (wrapper)
+        print("Hola")# instrucciones previas a la funcion decorada
+        function() # llamado a la funcion decorada
+        print("Chau") # instrucciones posteriores a la funcion decorada
         
-    return funcion_de_retorno
+    return funcion_de_retorno # retorno de la funcion de retorno
         
         
-@decorador_de_mensaje
-def mensaje():
+@decorador_de_mensaje # referencia a la funcion decoradora
+def mensaje(): # declaracion de una funcion a decorar
     print("Esto es programacion avanzada")
     
     
-mensaje()
-
+mensaje() # llamada a la funcion decorada
+print("---------------------------------------")
+print("---------------------------------------")
+print()
+print("Ejercicio 2.b")
 
 # EJERCICIO 2 b
 
+#declaracion de la funcion decoradora
 def decorador_dividir(function):
     
-    def funcion_de_retorno(*args,**kwargs):
-        print(kwargs)
-        print(kwargs.values())
-        if args[1]!=0:
+    def funcion_de_retorno(*args,**kwargs): # funcion de envoltura con toma de parametros
+        print("Numerador : "+str(args[0])+ " Denominador : "+str(args[1])) # impresion de los parametros l
+        
+        if args[1]!=0:# condicional para ejecutar o no la funcion decorada
             
-            resultado = dividir()
-            return resultado
-        else:
+            resultado = function(*args) # asignamos a una variable el resultado que retorna la funcion decorada
+            return resultado# retornamos el resultado
+        else:# codigo a ejecutar en caso de que el denominador sea cero
             print("no se puede dividir por cero")
-            
-    return funcion_de_retorno
+        print(" calculo finalizado")    
+    return funcion_de_retorno# retorno de la funcion de retorno
     
         
-@decorador_dividir
-def dividir(numerador, denominador):
+@decorador_dividir # referencia a la funcion decoradora
+def dividir(numerador, denominador): # funcion decorada
     return numerador/denominador
 
 print("--------------------")
 a = dividir(10,5)
 print(a)
+print("----------------")
+b= dividir(10,0)
+print(b)
+
+print("---------------------------------------")
+print("---------------------------------------")
+print()
+print("Ejercicio 2.c")
+
+
+# declaracion de la funcion decoradora
+
+def funcion_decoradora_fecha(function):
+    import datetime #importacion de datetime para extraer la fecha
+    import time # importacion de time para pausar el flujo de la funcion 
+    
+    def envoltura():# definicion de la funcion de retorno o envultura (wrapper)
+        print("se hizo un llamado a la funcion en la fecha : "+ str(datetime.datetime.now()))# impresion de la fecha en la que se llama a la funcion decorada
+        function() # llamado a la funcion decorada
+        for i in range(25):
+            print("%",end='')
+            time.sleep(0.5)
+        print()
+        print("espere un momento")
+        time.sleep(1)
+        print("la funcion finalizo en la fecha : "+ str(datetime.datetime.now()))# impresion de la fecha en la que finaliza la funcion decorada
+    return envoltura # retorno de la funcion de envoltura
+
+@funcion_decoradora_fecha# referencia a la funcion decoradora
+def hacer_algo(): # declaracion de la 
+    print(" estoy haciendo algo")
+    
+hacer_algo()
+
+
+print("---------------------------------------")
+print("---------------------------------------")
+print()
+print("Ejercicio 2.d.1")
+
+# declaramos la primera funcion decoradora
+def funcion_decoradora_1(function):
+    def wrapper():
+        print("dentro de la funcion decoradora 1")
+        function()
+        
+    return wrapper
+
+# declaramos la segunda funcion decoradora
+def funcion_decoradora_2(function):
+    def wrapper():
+        print("dentro de la funcion decoradora 2")
+        function()
+        
+    return wrapper
+
+@funcion_decoradora_1 # referenciamos al primer decorador
+@funcion_decoradora_2 # referenciamos al segundo decorador
+def funcion_base():   # definimos la funcio base
+    print("soy la funcion base")
+    
+funcion_base() # llamamos a la funcion
+
+# observacion: si bien se puede ver que se ejecutan los dos decoradores, la funcion base solo se ejecuta en uno
+
+
+print("---------------------------------------")
+print("---------------------------------------")
+print()
+print("Ejercicio 2.d.2")
+
+from decorador_externo import decorador_externo
+
+@decorador_externo
+def funcion_base_b():
+    print(" soy la funcion base")
+
+funcion_base_b()
--- metaclases.py
+++ metaclases.py
@@ -1,5 +1,16 @@
-# Ejercicio 1: rehacer tres clases del ejercicio de la clase anterior
+# PONCE ALVARO
+# PONCE FRANCISCO
 
+
+
+# Ejercicio 1.a: rehacer tres clases del ejercicio de la clase anterior
+
+'''
+EJERCICIO 1.B:
+                LOS METODOS SE PUEDEN DEFINIR DE DOS MANERAS EN LA CREACION DE UNA CLASE MEDIANTE UNA METACLASE,
+                UNA MANERA ES DENTRO DEL DICCIONARIO ASIGNANDOLE UNA CLAVE A UNA FUNCION LAMBDA.
+                OTRA MANERA ES DECLARARLO FUERA DEL METODO TYPE() Y ASIGNARLE UNA CLAVE DE REFERENCIA EN EL DICCIONARIO DEL METODO TYPE()
+'''
 # clase cancion
 #los metodos se definen por fuera de la clase
 def get_titulo(self):
@@ -13,9 +24,77 @@ def set_titulo(self, nuevo_titulo):
 
 def set_autor(self, nuevo_autor):
     self.autor = nuevo_autor
-
-Cancion =  type('Cancion',(),{'titulo':'---', 'autor':'---', 'get_titulo':get_titulo, 'get_autor':get_autor, 'set_titulo':set_titulo, 'set_autor':set_autor})
-
+# definimos una clase mediante el metodo type
+Cancion =  type('Cancion',(),{'titulo':'estoy verde', 'autor':'Charly', 'get_titulo':get_titulo, 'get_autor':get_autor, 'set_titulo':set_titulo, 'set_autor':set_autor})
+# instanciamos un objeto de la clase creada anteriormente
 cancion1= Cancion()
+# imprimimos los valores del objeto por defecto
+print(cancion1.get_titulo(), cancion1.autor)
+#seteamos el atributo titulo del objeto creado mediante el metodo set_titulo
+cancion1.set_titulo("Yendo de la cama al living")
+# imprimimos el objeto con un atribujo seteado
+print(cancion1.get_titulo(), cancion1.autor)
 
-print(cancion1.get_titulo, cancion1.autor)
+# clase persona
+
+# definicion externa de los metodos que va a usar la clase creada con type()
+def get_nombre(self):
+    return self.nombre
+    
+def get_apellido(self):
+    return self.apellido
+    
+def get_nacionalidad(self):
+    return self.nacionalidad
+    
+def set_nombre(self, nuevo_nombre):
+    self.nombre=nuevo_nombre
+        
+def set_apellido(self, nuevo_apellido):
+    self.apellido=nuevo_apellido
+    
+def set_nacionalidad(self, nueva_nacionalidad):
+    self.nacionalidad=nueva_nacionalidad
+    
+# definimos la clase mediante el metodo metaclase type()
+
+Persona = type('Persona',(),{'nombre':'--','apellido':'--', 'nacionalidad':'--', 'get_nombre':get_nombre, 'get_apellido': get_apellido, 'get_nacionalidad':get_nacionalidad, 'set_nombre':set_nombre, 'set_apellido':set_apellido, 'set_nacionalidad':set_nacionalidad})
+
+# creamos un objeto con la nueva clase Persona
+
+persona1= Persona()
+persona1.set_nombre("Juan")
+persona1.set_apellido("Peres")
+persona1.set_nacionalidad("argentino")
+print(persona1.get_nombre(), persona1.get_apellido(), persona1.get_nacionalidad())
+
+
+# clase punto
+
+def Impresion(self):
+    print("el punto se encuentra en la coordenada x={0} coordenada y={1}".format( str(self.x), str(self.y)))
+
+def Opuesto(self):
+    return "el punto se encuentra en la coordenada x={0} coordenada y={1}".format( str(self.x*-1), str(self.y*-1))
+
+def get_x(self):
+    return self.x
+def get_y(self):
+    return self.y
+def set_x(self, x):
+    self.x=x
+def set_y(self, y):
+    self.y=y
+
+Punto = type('Punto',(),{'x':0, 'y':0,'get_x':get_x, 'get_y':get_y,'set_x':set_x,'set_y':set_y, 'impresion':Impresion, 'opuesto':Opuesto})
+a= Punto()
+
+a.impresion()
+a.set_x(2)
+a.set_y(4)
+print()
+print(a.opuesto())
+        
+
+
+
